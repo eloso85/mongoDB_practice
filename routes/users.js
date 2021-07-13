20aa1e0e148f0c51f63adc064d31955344b722e1
Alejandro <eloso85@gmail.com>
2021-07-12 23:46:11 -0500

    finshed all the routes

--- routes/users.js
+++ routes/users.js
@@ -39,11 +39,45 @@ router.get('/:id', getUser,(req, res)=>{
 
 // Update one User
 router.patch('/:id', getUser, async(req, res)=>{
+    if(req.body.id != null){
+        res.user.id = req.body.id
+    }
+
+    if(req.body.fname != null){
+        res.user.fname = req.body.fname
+    }
+    if(req.body.lname != null){
+        res.user.lname = req.body.lname
+    }
+    if(req.body.username != null){
+        res.user.username = req.body.username
+    }
+
+    if(req.body.email != null){
+        res.user.email = req.body.email
+    }
+
+    if(req.body.avatar != null){
+        res.user.avatar = req.body.avatar
+    }
+
+    try {
+        const updatedUser = await res.user.save()
+        res.json(updatedUser)
+    } catch (err) {
+        res.status(400).json({message: err.message})
+        
+    }
 });
 
 // Delete one User
 router.delete('/:id', getUser, async(req, res)=>{
-
+    try {
+       await res.user.remove()
+       res.json({ message: " Deleted This User"}) 
+    } catch (error) {
+        res.status(500).json({ message: err.message})
+    }
 })
 
 //This is the middle ware
